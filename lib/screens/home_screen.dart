fc146ac5c5e560012b68676b991ae0a8797d5d44
Edd-war <digiespirit.1807@gmail.com>
2021-10-02 14:00:09 -0500

    Swiper - Carrusel de tarjetas
    Se crea el carrusel de imágenes y la creación de la carpeta de recursos "assets"

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:peliculas_app/widgets/widgets.dart';
-class HomeScreen extends StatelessWidget {
 
+class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        centerTitle: true,
         title: Text('Películas en Cines'),
         elevation: 0,
         actions: [
