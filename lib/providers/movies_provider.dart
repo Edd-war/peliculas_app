74ccb16b5c009fec07adf5b4545c6860d581b88d
Edd-war <digiespirit.1807@gmail.com>
2021-10-23 02:13:23 -0500

    Peticiones HTTP
    Se crea el modelo del objeto de la peliculas que serámn extraídas de la API

--- lib/providers/movies_provider.dart
+++ lib/providers/movies_provider.dart
@@ -1,6 +1,13 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class MoviesProvider extends ChangeNotifier{
+
+  String _apiKey ='58f349bbec836eb21ee528cd6fdd02d8';
+  String _baseUrl ='api.themoviedb.org';
+  String _language ='es-MX';
 
   MoviesProvider(){
     print('MoviesProviderInicializado');
@@ -8,6 +15,18 @@ class MoviesProvider extends ChangeNotifier{
   }
 
   getOnDisplayMovies() async {
-    print('getOnDisplayMovies');
+    var url = Uri.https(_baseUrl, '/3/movie/popular', {
+      'api_key' : _apiKey,
+      'language' : _language,
+      'page' : '1'
+    });
+    // Await the http response, then decode the json-formatted response
+    final response = await http.get(url);
+    final Map<String, dynamic> decodeData = json.decode(response.body);
+   // if (response.statusCode!=200) return ('ERROR');
+
+    print(decodeData['results']);
+
   }
+
 }
