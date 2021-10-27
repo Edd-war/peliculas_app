79d45afbcf822148237f75fd4b4e0cc18c41d507
Edd-war <digiespirit.1807@gmail.com>
2021-10-27 11:26:32 -0500

    Tarea - Mostrar películas populares
    Se hace el despliegue de la información obtenida de la API en la interfaz de la App

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -27,7 +27,10 @@ class HomeScreen extends StatelessWidget {
             CardSwiper(movies: moviesProvider.onDisplayMovies),
 
             // Movie Slider
-            MovieSlider(),
+            MovieSlider(
+              movies: moviesProvider.popularMovies,
+              title: 'Populares',
+            ),
             // MovieSlider(),
             // MovieSlider(),
             // MovieSlider(),
--- lib/widgets/movie_slider.dart
+++ lib/widgets/movie_slider.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:peliculas_app/models/models.dart';
 
 class MovieSlider extends StatelessWidget {
-
+  final List<Movie> movies;
+  final String? title;
+  const MovieSlider({
+    Key? key,
+    required this.movies,
+    this.title
+  }):super(key: key);
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -12,16 +19,17 @@ class MovieSlider extends StatelessWidget {
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: 20),
-            child: Text('Populares', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),)
-          ),
+          if(this.title!= null)
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20),
+              child: Text(this.title!, style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),)
+            ),
           SizedBox(height: 5,),
           Expanded(
             child: ListView.builder(
               scrollDirection: Axis.horizontal,
-              itemCount: 20,
-              itemBuilder: ( _, int index)=> _MoviePoster(),
+              itemCount: movies.length,
+              itemBuilder: ( _, int index)=> _MoviePoster(movies[index]),
             ),
           )
         ],
@@ -32,7 +40,8 @@ class MovieSlider extends StatelessWidget {
 
 
 class _MoviePoster extends StatelessWidget {
-
+  final Movie movie;
+  const _MoviePoster(this.movie);
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -48,7 +57,7 @@ class _MoviePoster extends StatelessWidget {
               borderRadius: BorderRadius.circular(20),
               child: FadeInImage(placeholder: AssetImage(
                 'assets/no-image.jpg'),
-                image: NetworkImage('https://via.placeholder.com/300x400'),
+                image: NetworkImage(movie.fullPosterImg),
                 width: 130,
                 height: 190,
                 fit: BoxFit.cover,
@@ -59,7 +68,7 @@ class _MoviePoster extends StatelessWidget {
             height: 5,
           ),
           Text(
-            'StarWars: El retornod',
+            movie.title,
             maxLines: 2,
             overflow: TextOverflow.ellipsis,
             textAlign: TextAlign.center,
