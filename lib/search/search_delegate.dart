9c5da890a19506055b7156abcc1a344ef961abaf
Edd-war <digiespirit.1807@gmail.com>
2021-10-28 02:20:33 -0500

    Mostrar resultados del Stream

--- lib/search/search_delegate.dart
+++ lib/search/search_delegate.dart
@@ -56,6 +56,7 @@ class MovieSearchDelegate extends SearchDelegate{
       return _emptyContainer();
     print('Petición HTTP request');
     final moviesProvider = Provider.of<MoviesProvider>(context, listen: false);
+    moviesProvider.getSuggestionsByQuery(query);
     return StreamBuilder(
       stream: moviesProvider.suggestionStream,
       builder: (_, AsyncSnapshot<List<Movie>> snapshot){
